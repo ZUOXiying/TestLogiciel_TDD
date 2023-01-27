928017cfa045de8d6d4a1dacca2ad3d694f66368
ZUOXiying <xdx_zuo@163.com>
2023-01-27 14:33:57 +0100

    add si_geometrique test

---
+++ tests_funcs.py
@@ -0,0 +1,20 @@
+"""Test Logiciel TDD"""
+import unittest
+import funcs
+
+
+class TestFuncs(unittest.TestCase):
+    """test functions"""
+    def test_geometrique(self):
+        """test si c’est une suite géométrique"""
+        self.assertTrue(funcs.si_geometrique([1,2,4,8,16]))
+        self.assertTrue(funcs.si_geometrique([0,0,0,0,0]))
+        self.assertTrue(funcs.si_geometrique([-1,1,-1,1,-1]))
+        self.assertTrue(funcs.si_geometrique([16,8,4,2,1]))
+
+        self.assertFalse(funcs.si_geometrique([1,2,3,4,5]))
+        self.assertFalse(funcs.si_geometrique([2,4,6,8,10]))
+        self.assertFalse(funcs.si_geometrique([1,-1,1,-1,-1]))
+        self.assertFalse(funcs.si_geometrique([1,0,1,0,1]))
+if __name__ == '__main__':
+    unittest.main()
