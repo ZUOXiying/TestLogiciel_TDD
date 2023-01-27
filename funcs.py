93d6b5e9b8c30258caa2a8acccf76cbd9df9de9b
ZUOXiying <xdx_zuo@163.com>
2023-01-27 15:44:27 +0100

    add si_arithmetique function

--- funcs.py
+++ funcs.py
@@ -13,3 +13,15 @@ def si_geometrique(list_geo):
         return True
     except ZeroDivisionError:
         return False
+
+def si_arithmetique(list_ari):
+    """si c’est une suite arithmétique"""
+    numbre = len(list_ari)
+    if numbre == 1:
+        return False
+    d_list = list_ari[1]-list_ari[0]
+    for i in range(1,numbre):
+        temp = list_ari[i]-list_ari[i-1]
+        if temp != d_list:
+            return False
+    return True
