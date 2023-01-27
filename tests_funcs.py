4014211de8d355deeaa360b325547d19b4a91f39
ZUOXiying <xdx_zuo@163.com>
2023-01-27 16:55:04 +0100

    add test: si arithmetique et renvoie la liste des n éléments suivant

--- tests_funcs.py
+++ tests_funcs.py
@@ -36,5 +36,15 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.geometrique_plus(1, [1,2,3,4,5]), False)
         self.assertEqual(funcs.geometrique_plus(0, [2,4,6,8,10]), False)
         self.assertEqual(funcs.geometrique_plus(2, [0,1,0,1,0]), False)
+
+    def test_arithmetique_plus(self):
+        """test si la suite est arithmétique et renvoie la liste des n éléments suivant"""
+        self.assertEqual(funcs.arithmetique_plus(3, [1,2,3,4,5]), [True, [6,7,8]])
+        self.assertEqual(funcs.arithmetique_plus(2, [1,1,1,1,1]), [True, [1,1]])
+        self.assertEqual(funcs.arithmetique_plus(3, [-1,1,3,5,7]), [True, [9,11,13]])
+        self.assertEqual(funcs.arithmetique_plus(0, [16,12,8,4,0]), [True, []])
+        self.assertEqual(funcs.arithmetique_plus(1, [1,4,9,16,25]), False)
+        self.assertEqual(funcs.arithmetique_plus(0, [-1,1,-1,1,-1]), False)
+        self.assertEqual(funcs.arithmetique_plus(2, [2,4,6,8,11]), False)
 if __name__ == '__main__':
     unittest.main()
